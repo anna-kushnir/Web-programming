4122c21eaba4c7c539cc6eae2738d3cd10d142f1
Ганна Кушнір <anna.kushnir.1008@gmail.com>
2022-12-28 19:34:23 +0200

    додано зчитування всіх елементів форми

--- lab6/main.js
+++ lab6/main.js
@@ -59,27 +59,43 @@ function submitForm(e) {
     e.preventDefault();
     let form1 = e.target.parentElement;
     let stack = document.querySelector(".stack");
-    let stack1 = document.querySelector("#stack1");
-    let stack2 = document.querySelector("#stack2");
-    let stack3 = document.querySelector("#stack3");
-    
+
     let font_size = form1.querySelector("#font_size").value + "px";
     let bold_text = form1.querySelector("#bold_text").checked;
     let italic_text = form1.querySelector("#italic_text").checked;
     let text_color = form1.querySelector("#text_color").value;
     let first_glitch_color = form1.querySelector("#first_glitch_color").value;
     let second_glitch_color = form1.querySelector("#second_glitch_color").value;
-    let padding_left = parseInt(form1.querySelector("#padding_left").value);
-    let padding_right = parseInt(form1.querySelector("#padding_right").value);
-    let left_aligh = form1.querySelector("#left_align").checked;
-    let center_aligh = form1.querySelector("#center_align").checked;
-    let right_aligh = form1.querySelector("#right_align").checked;
-    let open_animation_duration = parseInt(form1.querySelector("#open_animation_duration").value);
-    let animation_duration = parseInt(form1.querySelector("#animation_duration").value);
+    let padding_left = form1.querySelector("#padding_left").value + "px";
+    let padding_right = form1.querySelector("#padding_right").value + "px";
+    let left_align = form1.querySelector("#left_align").checked;
+    let center_align = form1.querySelector("#center_align").checked;
+    let open_animation_duration = form1.querySelector("#open_animation_duration").value + "ms";
+    let animation_duration = form1.querySelector("#animation_duration").value + "ms";
 
-    stack1.style.fontSize = font_size;
-    stack2.style.fontSize = font_size;
-    stack3.style.fontSize = font_size;
+    stack.style.fontSize = font_size;
+    if (bold_text) {
+        stack.style.fontWeight = "bold";
+    }
+    if (italic_text) {
+        stack.style.fontStyle = "italic";
+    }
+    stack.style.color = text_color;
+    stack.style.setProperty("--first-glitch-color", first_glitch_color);
+    stack.style.setProperty("--second-glitch-color", second_glitch_color);
+    stack.style.setProperty("--padding-left", padding_left);
+    stack.style.setProperty("--padding-right", padding_right);
+    if (left_align) {
+        stack.style.textAlign = "left";
+    }
+    else if (center_align) {
+        stack.style.textAlign = "center";
+    }
+    else {
+        stack.style.textAlign = "right";
+    }
+    stack.style.setProperty("--open-animation-duration", open_animation_duration);
+    stack.style.setProperty("--animation-duration", animation_duration);
 }
 let save_button = document.querySelector("#save_btn");
 save_button.addEventListener("click", submitForm);
